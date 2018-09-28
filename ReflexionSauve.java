5c7f3ea024efa5ddef94862ed0f7e7c01e524616
uchiwayas <yassine.tahri@outlook.fr>
2018-09-28 15:19:29 +0200

    c moch

--- ReflexionSauve.java
+++ ReflexionSauve.java
@@ -12,34 +12,88 @@ public class ReflexionSauve {
 	public static void main(String[] args) {
 		
 		Point p = new Point(2,3);
-		Personne per = new Personne("Yassine", "Tahri", 175, 76, 'm');
+		//Personne per = new Personne("Yassine", "Tahri", 175, 76, 'm');
 		Cercle c = new Cercle(p, 30);
 		
-		String fichier = "monpoint1.txt";
-		String fichier2 = "mapersonne1.txt";
+		//String fichier = "monpoint1.txt";
+		//String fichier2 = "mapersonne1.txt";
 		String fichier3 = "moncercle1.txt";
 		
-		sauve(p, fichier);
-		sauve(per, fichier2); 
-		sauve(c, fichier3); 
+		//sauveRecursif(p, fichier);
+		//sauveRecursif(per, fichier2); 
+		sauveRecursif(c, fichier3);
+		
+//		sauveSimple(p, fichier);
+//		sauveSimple(per, fichier2); 
+//		sauveSimple(c, fichier3); 
 	}
 	
-	public static void sauve(Object objet, String nomFichier){
+//	public static void sauveSimple(Object objet, String nomFichier){
+//		Class<?> cl = objet.getClass();
+//		Field[] champs = cl.getDeclaredFields();
+//		try {
+//			PrintWriter writer = new PrintWriter(nomFichier, "UTF-8");
+//			writer.println("Données pour créer une instance de : "+ objet.getClass().getName());
+//			for (Field ch : champs){
+//				try {
+//					//on crée un string de getter (ex : getX)
+//					String getterName = créeNomGetter(ch.getName());
+//					//on crée un methode m, et on lui attribue la VRAIE methode getterName (la vraie getX)
+//					Method m = cl.getMethod(getterName);
+//					//on utilise la methode grace a invoke sur l'objet pour récuperer la valeur
+//					Object obj = m.invoke(objet);
+//					writer.println(ch.getName() + "=" + obj);
+//					System.out.println(ch.getName() + "=" + obj);
+//				} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException
+//						| InvocationTargetException e) {
+//					// On ne fait rien
+//				}
+//			}
+//			writer.close();
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (UnsupportedEncodingException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}		
+//	}
+	
+	public static String sauveRecursif(Object objet, String nomFichier){
 		Class<?> cl = objet.getClass();
 		Field[] champs = cl.getDeclaredFields();
 		try {
 			PrintWriter writer = new PrintWriter(nomFichier, "UTF-8");
 			writer.println("Données pour créer une instance de : "+ objet.getClass().getName());
+			writer.println(objet.getClass());
 			for (Field ch : champs){
 				try {
-					//on crée un string de getter (ex : getX)
-					String getterName = créeNomGetter(ch.getName());
-					//on crée un methode m, et on lui attribue la VRAIE methode getterName (la vraie getX)
-					Method m = cl.getMethod(getterName);
-					//on utilise la methode grace a invoke sur l'objet pour récuperer la valeur
-					Object obj = m.invoke(objet);
-					writer.println(ch.getName() + "=" + obj);
-					System.out.println(ch.getName() + "=" + obj);
+					System.out.println("ch = " + ch.getName());
+					String classe = ch.getType().toString();
+					System.out.println("classe = " + classe);
+				   if (classe.trim().contains("exoReflexion")) { //getDeclaringClass
+						int id = (int) (Math.random() * (100));
+						String fichier = "truc"+id+".txt"; //getDeclaringClass
+						
+						String getterName = créeNomGetter(ch.getName());
+						//on crée un methode m, et on lui attribue la VRAIE methode getterName (la vraie getX)
+						Method m = cl.getMethod(getterName);
+						//on utilise la methode grace a invoke sur l'objet pour récuperer la valeur
+						Object obj = m.invoke(objet);
+						
+						writer.println(ch.getName() + "=" + sauveRecursif(obj, fichier));
+						//System.out.println(ch.getName() + "=" + sauveRecursif(ch, fichier));
+				   }
+				   else {
+						//on crée un string de getter (ex : getX)
+						String getterName = créeNomGetter(ch.getName());
+						//on crée un methode m, et on lui attribue la VRAIE methode getterName (la vraie getX)
+						Method m = cl.getMethod(getterName);
+						//on utilise la methode grace a invoke sur l'objet pour récuperer la valeur
+						Object obj = m.invoke(objet);
+						writer.println(ch.getName() + "=" + obj);
+						//System.out.println(ch.getName() + "=" + obj);
+				   }
 				} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException
 						| InvocationTargetException e) {
 					// On ne fait rien
@@ -52,7 +106,9 @@ public class ReflexionSauve {
 		} catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}		
+		}
+		
+		return nomFichier;
 	}
 
 	public static String créeNomGSetter(String getouset, String nomChamp)
